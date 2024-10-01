362348f0d708770523e9cb95e826a977607d25b2
asu01-tech <asuseylkhanova@gmail.com>
2024-10-01 20:19:23 +0500

    first commit

--- stack_visualizer.py
+++ stack_visualizer.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 
-def on_button_click()
-Label.config(text="batyrmany bas")
+def on_button_click():
+    label.config(text="batyrmany basu")
 
-root=tk.TK()
+root = tk.Tk()
 root.title("stack")
 root.geometry("600x500")
 
-button=tk.button(root, text="bastau", command=on_button_click)
-button.pack
+button = tk.Button(root, text="bastau", command=on_button_click)
+button.pack()
 
 root.mainloop()
