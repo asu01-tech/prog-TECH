d2b291d60c36595a0c25f263a2bfd43dee37d7bc
asu01-tech <asuseylkhanova@gmail.com>
2024-10-02 07:29:40 +0500

    first commit

--- stack_visualizer.py
+++ stack_visualizer.py
@@ -10,10 +10,21 @@ def on_button_click():
     second_label=tk.Label(second_window, text="ekinshi tereze")
     second_label.pack(pady=20)
 
+def on_submit():
+    try:
+        num_elements = int(entry.get())
+        if num_elements <= 0:
+            raise ValueError("element sany on boluy kerek")
+
 
 root = tk.Tk()
 root.title("stack")
 root.geometry("600x500")
+
+stack= []
+
+submit_button = tk.Button(root, text="Ondeu", command=on_submit)
+submit_button.pack(pady=10)
 
 label = tk.Label(root, text="batyrmany basu")
 label.pack(pady=20)
